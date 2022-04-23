0982bda1a48f267b60ea34edbec6d2853097224d
Julia Gongala <65823190+jgongala@users.noreply.github.com>
2022-04-23 14:27:41 +0100

    Step 2
    updated code for step 2 requirements

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,10 +1,12 @@
+import java.util.LinkedList;
+
 public class FamilyTree {
 	
 	private int uniqueIdentifier = 1;
+
 	private FamilyTreeNode ancestor;
-	
-	//TODO adding all members for list easy search
-	
+	private LinkedList <FamilyTreeNode> familyList = new LinkedList();
+		
 	private class FamilyTreeNode {
 		private int identifier;
 		private String name;
@@ -14,83 +16,164 @@ public class FamilyTree {
 		
 		public FamilyTreeNode() {
 			this.identifier = uniqueIdentifier;
-			uniqueIdentifier++;
 		}
+
+		public int getIdentifier() {
+			return identifier;
+		}
+		
 	}
 	
 	public class UniqueNameConstraintException extends Exception {}
 	public class FamilyMemberNotFoundException extends Exception {}
-
-	
-	
+	public class PartnerNeededException extends Exception{};
+	public class FamilyMemberHasPartnerException extends Exception{};
 	
 	public FamilyTree(String ancestorName) {
 		this.ancestor = new FamilyTreeNode();
 		this.ancestor.name = ancestorName;
+		familyList.add(ancestor);
+		uniqueIdentifier++;
 	}
 	
-	public void addPartner (String partnerName, int familyMemberId) throws FamilyMemberNotFoundException {
+	public void addPartner (String partnerName, int familyMemberId) throws FamilyMemberNotFoundException, FamilyMemberHasPartnerException {
 		FamilyTreeNode familyMember = findFamilyMember(familyMemberId);
 		
 		if (familyMember == null) {
 			throw new FamilyMemberNotFoundException();
+		} 
+		
+		if (familyMember.partner != null) {
+			throw new FamilyMemberHasPartnerException();
 		} else {
 			FamilyTreeNode partner = new FamilyTreeNode();
 			partner.name = partnerName;
 			partner.partner = familyMember;
 			familyMember.partner = partner;
+			familyList.add(partner);
+			uniqueIdentifier++;
 		}	
 	}
 	
-	public void addChild (String childName) throws UniqueNameConstraintException {
+	public void addChild (String childName) throws UniqueNameConstraintException, PartnerNeededException {
 		FamilyTreeNode child = new FamilyTreeNode();
 		child.name = childName;
 		
-		if (this.ancestor.firstChild == null) {
-			this.ancestor.firstChild = child;
-			this.ancestor.partner.firstChild = child;
+		if (this.ancestor.partner == null) {
+			throw new PartnerNeededException();
 		} else {
-			FamilyTreeNode next = this.ancestor.firstChild;
-			
-			//Checks first sibling name constraint
-			if(next.name.equalsIgnoreCase(childName)) {
-				throw new UniqueNameConstraintException();
-			}
-			while (next.nextSibling != null) {
-				//checks next sibling's name constraint
-				if(next.nextSibling.name.equalsIgnoreCase(childName)) {
+			if (this.ancestor.firstChild == null) {
+				this.ancestor.firstChild = child;
+				this.ancestor.partner.firstChild = child;
+			} else {
+				FamilyTreeNode next = this.ancestor.firstChild;
+				
+				//Checks first sibling name constraint
+				if(next.name.equalsIgnoreCase(childName)) {
 					throw new UniqueNameConstraintException();
 				}
-				next= next.nextSibling;
+				while (next.nextSibling != null) {
+					//checks next sibling's name constraint
+					if(next.nextSibling.name.equalsIgnoreCase(childName)) {
+						throw new UniqueNameConstraintException();
+					}
+					next = next.nextSibling;
+				}
+				next.nextSibling = child;
 			}
-			next.nextSibling = child;
 		}
+		familyList.add(child);
+		uniqueIdentifier++;
 	}
 	
 	public String toString() {
 		String familyDetails = new String();
 		
-		familyDetails += this.ancestor.name + "(identifier " + this.ancestor.identifier + ") partner " + this.ancestor.partner.name + "(identifier " + this.ancestor.partner.identifier + ")\n";
+		if (this.ancestor.partner == null) {
+			familyDetails += this.ancestor.name + "(identifier " + this.ancestor.identifier + ") has no partner\n";
+		} else {
+			familyDetails += this.ancestor.name + "(identifier " + this.ancestor.identifier + ") partner " + this.ancestor.partner.name + "(identifier " + this.ancestor.partner.identifier + ")\n";
+		}
+		
 		FamilyTreeNode familyMember = this.ancestor.firstChild;
 		if (familyMember == null) {
 			familyDetails += " has no children\n";
 		} else {
 			// TODO
 			while (familyMember != null) {
-				familyDetails += " " + familyMember.name + "(identifier " + familyMember.identifier + ")\n";
+				if(familyMember.partner == null) {
+					familyDetails += " " + familyMember.name + "(identifier " + familyMember.identifier + ") has no partner\n";
+				} else {
+					familyDetails += " " + familyMember.name + "(identifier " + familyMember.identifier + ") partner " + familyMember.partner.name + "(identifier " + familyMember.partner.identifier + ")\n";
+					familyDetails += getLegacyDetails(familyMember);
+				}
 				familyMember = familyMember.nextSibling;
 			}
 		}
 		
 		return familyDetails;
 	}
-
+	
+	private String getLegacyDetails(FamilyTreeNode familyMember) {
+		String legacyDetails = new String();
+		familyMember = familyMember.firstChild;
+		if (familyMember == null) {
+			legacyDetails += "  has no children\n";
+		} else {
+			while (familyMember != null) {
+				if(familyMember.partner == null) {
+					legacyDetails += " " + familyMember.name + "(identifier " + familyMember.identifier + ") has no partner\n";
+				} else {
+					legacyDetails += " " + familyMember.name + "(identifier " + familyMember.identifier + ") partner " + familyMember.partner.name + "(identifier " + familyMember.partner.identifier + ")\n";
+					legacyDetails += getLegacyDetails(familyMember);
+				}
+				familyMember = familyMember.nextSibling;
+			}
+		}
+		return legacyDetails;
+	}
+	
+	public String familyMemberDetails(int id) throws FamilyMemberNotFoundException {
+		String familyMemberDetails = new String();
+		FamilyTreeNode familyMember = findFamilyMember(id);
+		if(familyMember.partner == null) {
+			familyMemberDetails += familyMember.name + "(identifier " + familyMember.identifier + ") has no partner\n";
+		} else {
+			familyMemberDetails += familyMember.name + "(identifier " + familyMember.identifier + ") partner " + familyMember.partner.name + "(identifier " + familyMember.partner.identifier + ")\n";
+			familyMemberDetails += getLegacyDetails(familyMember);
+		}
+		
+		return familyMemberDetails;
+	}
+	
 	
 	public FamilyTreeNode findFamilyMember (int id) throws FamilyMemberNotFoundException {
 		
-		//TODO
+		for (FamilyTreeNode familyMember : familyList) {
+			if (familyMember.getIdentifier() == id) {
+				return familyMember;
+			}
+		}
 		throw new FamilyMemberNotFoundException();
-
+	}
+	
+	public void printMembers() {
+		for (FamilyTreeNode member : familyList) {
+			System.out.println(member.identifier + ". " + member.name);
+		}
+	}
+	
+	public void ifHasPartnerThrowException (int id) throws FamilyMemberNotFoundException, FamilyMemberHasPartnerException {
+		FamilyTreeNode member = findFamilyMember(id);
+		if (member.partner != null) {
+			throw new FamilyMemberHasPartnerException();
+		}
+	}
+	
+	public void ifAncestorHasPartnerThrowException() throws PartnerNeededException {
+		if (this.ancestor.partner == null) {
+			throw new PartnerNeededException();
+		}
 	}
 	
 }
--- src/FamilyTreeTest.java
+++ src/FamilyTreeTest.java
@@ -20,6 +20,7 @@ public class FamilyTreeTest {
 					System.out.println("invalid entry");
 				}
 			}
+			
 			switch (option) {
 				case 0:
 					if (Input.getString("Are you sure? (Y/N): ").equalsIgnoreCase("y")) {
@@ -28,14 +29,23 @@ public class FamilyTreeTest {
 					} else {
 						break;
 					}
+					
 				case 1:
-					String childName = Input.getString("Enter child's name: ");
 					try {
-						familyTree.addChild(childName);
+						try {
+							// there is an other way of doing this (if statement, that I would much rather use), 
+							// but to follow requirement of the assessment I used exception as required 
+							familyTree.ifAncestorHasPartnerThrowException();
+							String childName = Input.getString("Enter child's name: ");
+							familyTree.addChild(childName);
+						} catch (FamilyTree.PartnerNeededException e) {
+							System.out.println("Cannot add child to ancestor with no partner");
+						}
 					} catch (FamilyTree.UniqueNameConstraintException e) {
 						System.out.println("Sibling with this name already exists.");
 					}
 					break;
+					
 				case 2:
 					System.out.println("1: display whole family tree");
 					System.out.println("2: display specific family member");
@@ -46,8 +56,13 @@ public class FamilyTreeTest {
 							System.out.println(familyTree);
 							break;
 						case 2: 
-							// TODO
-							System.out.println("specific member");
+							familyTree.printMembers();
+							Integer membersFamilyToDisplay = Input.getInteger("Enter family member identifier that you want display: ");
+							try {
+								System.out.println(familyTree.familyMemberDetails(membersFamilyToDisplay));
+							} catch (FamilyTree.FamilyMemberNotFoundException e) {
+								System.out.println("member not found");
+							}
 							break;
 						default:
 							System.out.println("Please enter a valid option from the menu");
@@ -55,8 +70,23 @@ public class FamilyTreeTest {
 					
 					break;
 				case 3:
-					//TODO
-					System.out.println("Adding partner");
+					System.out.println(familyTree);
+					Integer memberIdentifier = Input.getInteger("Enter family member identifier that you want to add partner to: ");
+					
+					try {
+						// there is an other way of doing this (if statement, that I would much rather use), 
+						// but to follow requirement of the assessment I used exception as required 
+						familyTree.ifHasPartnerThrowException(memberIdentifier);
+						if (familyTree.findFamilyMember(memberIdentifier) != null) {
+							String partnerName = Input.getString("Partner's name: ");
+							familyTree.addPartner(partnerName, memberIdentifier);
+						}
+					
+					} catch (FamilyTree.FamilyMemberNotFoundException e) {
+						System.out.println("member not found");
+					} catch (FamilyTree.FamilyMemberHasPartnerException e) {
+						System.out.println("family member has already a partner");
+					}
 					break;
 				default:
 					System.out.println("Please enter a valid option from the menu");
